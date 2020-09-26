f7c7dc83a43046cd04f92f5a27fa77bc72351352
dlebansais <dlebansais@gmail.com>
2020-09-26 12:48:55 +0200

    Added support for tracing.

--- RegistryTools/Settings.cs
+++ RegistryTools/Settings.cs
@@ -1,10 +1,13 @@
 ﻿namespace RegistryTools
 {
     using System;
+    using System.Diagnostics;
     using System.Globalization;
     using System.IO;
+    using System.Runtime.CompilerServices;
     using System.Security;
     using Microsoft.Win32;
+    using Tracing;
 
     /// <summary>
     /// Represents settings stored in the registry.
@@ -21,8 +24,28 @@
         {
             RootName = rootName;
             ProductName = productName;
+            Tracer = null;
 
             OpenKey();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Settings"/> class.
+        /// </summary>
+        /// <param name="rootName">A root name to use for sets of products key, for example the company name.</param>
+        /// <param name="productName">A product name to use for all settings.</param>
+        /// <param name="tracer">A tracer for diagnostic purpose.</param>
+        public Settings(string rootName, string productName, ITracer tracer)
+        {
+            RootName = rootName;
+            ProductName = productName;
+            Tracer = tracer;
+
+            TraceStart();
+
+            OpenKey();
+
+            TraceEnd();
         }
         #endregion
 
@@ -36,6 +59,11 @@
         /// Gets the product name.
         /// </summary>
         public string ProductName { get; }
+
+        /// <summary>
+        /// Gets the tracer.
+        /// </summary>
+        public ITracer? Tracer { get; }
         #endregion
 
         #region Client Interface
@@ -45,8 +73,12 @@
         /// </summary>
         public void RenewKey()
         {
+            TraceStart();
+
             CloseKey();
             OpenKey();
+
+            TraceEnd();
         }
 
         /// <summary>
@@ -56,7 +88,13 @@
         /// <returns>True if the value is set; otherwise, false.</returns>
         public bool IsValueSet(string valueName)
         {
-            return GetSettingKey(valueName) != null;
+            TraceStart();
+
+            bool Result = GetSettingKey(valueName, isExistQuery: true) != null;
+
+            TraceEnd();
+
+            return Result;
         }
 
         /// <summary>
@@ -68,18 +106,25 @@
         /// <returns>True if found in the registry; otherwise, false.</returns>
         public bool GetBool(string valueName, bool defaultValue, out bool value)
         {
+            TraceStart();
+
+            bool Result;
             int? KeyValue = GetSettingKey(valueName) as int?;
 
             if (KeyValue.HasValue)
             {
                 value = KeyValue.Value != 0;
-                return true;
+                Result = true;
             }
             else
             {
                 value = defaultValue;
-                return false;
+                Result = false;
             }
+
+            TraceEnd();
+
+            return Result;
         }
 
         /// <summary>
@@ -90,7 +135,11 @@
         /// <param name="value">The value to set.</param>
         public void SetBool(string valueName, bool value)
         {
+            TraceStart();
+
             SetSettingKey(valueName, value ? 1 : 0, RegistryValueKind.DWord);
+
+            TraceEnd();
         }
 
         /// <summary>
@@ -102,18 +151,25 @@
         /// <returns>True if found in the registry; otherwise, false.</returns>
         public bool GetInt(string valueName, int defaultValue, out int value)
         {
+            TraceStart();
+
+            bool Result;
             int? KeyValue = GetSettingKey(valueName) as int?;
 
             if (KeyValue.HasValue)
             {
                 value = KeyValue.Value;
-                return true;
+                Result = true;
             }
             else
             {
                 value = defaultValue;
-                return false;
+                Result = false;
             }
+
+            TraceEnd();
+
+            return Result;
         }
 
         /// <summary>
@@ -124,7 +180,11 @@
         /// <param name="value">The value to set.</param>
         public void SetInt(string valueName, int value)
         {
+            TraceStart();
+
             SetSettingKey(valueName, value, RegistryValueKind.DWord);
+
+            TraceEnd();
         }
 
         /// <summary>
@@ -136,18 +196,25 @@
         /// <returns>True if found in the registry; otherwise, false.</returns>
         public bool GetString(string valueName, string defaultValue, out string value)
         {
+            TraceStart();
+
+            bool Result;
             string? KeyValue = GetSettingKey(valueName) as string;
 
             if (KeyValue != null)
             {
                 value = KeyValue;
-                return true;
+                Result = true;
             }
             else
             {
                 value = defaultValue;
-                return false;
+                Result = false;
             }
+
+            TraceEnd();
+
+            return Result;
         }
 
         /// <summary>
@@ -158,10 +225,14 @@
         /// <param name="value">The value to set.</param>
         public void SetString(string valueName, string value)
         {
+            TraceStart();
+
             if (value == null)
                 DeleteSetting(valueName);
             else
                 SetSettingKey(valueName, value, RegistryValueKind.String);
+
+            TraceEnd();
         }
 
         /// <summary>
@@ -173,15 +244,22 @@
         /// <returns>True if found in the registry; otherwise, false.</returns>
         public bool GetFloat(string valueName, float defaultValue, out float value)
         {
+            TraceStart();
+
+            bool Result;
             string? KeyValue = GetSettingKey(valueName) as string;
 
             if (KeyValue != null && float.TryParse(KeyValue, NumberStyles.Float, CultureInfo.InvariantCulture, out value))
-                return true;
+                Result = true;
             else
             {
                 value = defaultValue;
-                return false;
+                Result = false;
             }
+
+            TraceEnd();
+
+            return Result;
         }
 
         /// <summary>
@@ -192,8 +270,12 @@
         /// <param name="value">The value to set.</param>
         public void SetFloat(string valueName, float value)
         {
+            TraceStart();
+
             string StringValue = value.ToString(CultureInfo.InvariantCulture);
             SetSettingKey(valueName, StringValue, RegistryValueKind.String);
+
+            TraceEnd();
         }
 
         /// <summary>
@@ -205,15 +287,22 @@
         /// <returns>True if found in the registry; otherwise, false.</returns>
         public bool GetDouble(string valueName, double defaultValue, out double value)
         {
+            TraceStart();
+
+            bool Result;
             string? KeyValue = GetSettingKey(valueName) as string;
 
             if (KeyValue != null && double.TryParse(KeyValue, NumberStyles.Float, CultureInfo.InvariantCulture, out value))
-                return true;
+                Result = true;
             else
             {
                 value = defaultValue;
-                return false;
+                Result = false;
             }
+
+            TraceEnd();
+
+            return Result;
         }
 
         /// <summary>
@@ -224,8 +313,12 @@
         /// <param name="value">The value to set.</param>
         public void SetDouble(string valueName, double value)
         {
+            TraceStart();
+
             string StringValue = value.ToString(CultureInfo.InvariantCulture);
             SetSettingKey(valueName, StringValue, RegistryValueKind.String);
+
+            TraceEnd();
         }
 
         /// <summary>
@@ -237,15 +330,22 @@
         /// <returns>True if found in the registry; otherwise, false.</returns>
         public bool GetGuid(string valueName, Guid defaultValue, out Guid value)
         {
+            TraceStart();
+
+            bool Result;
             string? KeyValue = GetSettingKey(valueName) as string;
 
             if (KeyValue != null && Guid.TryParse(KeyValue, out value))
-                return true;
+                Result = true;
             else
             {
                 value = defaultValue;
-                return false;
+                Result = false;
             }
+
+            TraceEnd();
+
+            return Result;
         }
 
         /// <summary>
@@ -256,62 +356,111 @@
         /// <param name="value">The value to set.</param>
         public void SetGuid(string valueName, Guid value)
         {
+            TraceStart();
+
             string StringValue = value.ToString();
             SetSettingKey(valueName, StringValue, RegistryValueKind.String);
+
+            TraceEnd();
         }
         #endregion
 
         #region Implementation
         private void OpenKey()
         {
+            TraceStart();
+
             RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"Software", true);
             Key = Key.CreateSubKey(RootName);
             SettingKey = Key.CreateSubKey(ProductName);
+
+            TraceEnd();
         }
 
         private void CloseKey()
         {
+            TraceStart();
+
             using (RegistryKey? Key = SettingKey)
             {
                 SettingKey = null;
             }
+
+            TraceEnd();
         }
 
-        private object? GetSettingKey(string valueName)
+        private object? GetSettingKey(string valueName, bool isExistQuery = false)
         {
+            TraceStart();
+
+            object? Result;
+
             try
             {
-                return SettingKey?.GetValue(valueName);
+                Result = SettingKey?.GetValue(valueName);
             }
             catch (Exception e) when (e is SecurityException || e is ObjectDisposedException || e is IOException || e is UnauthorizedAccessException)
             {
-                return null;
+                if (!isExistQuery)
+                    Tracer?.Write(Category.Debug, e, "Failed to query value");
+
+                Result = null;
             }
+
+            TraceEnd();
+
+            return Result;
         }
 
         private void SetSettingKey(string valueName, object value, RegistryValueKind kind)
         {
+            TraceStart();
+
             try
             {
                 SettingKey?.SetValue(valueName, value, kind);
             }
             catch (Exception e) when (e is SecurityException || e is ObjectDisposedException || e is IOException || e is UnauthorizedAccessException)
             {
+                Tracer?.Write(Category.Debug, e, "Failed to set value");
             }
+
+            TraceEnd();
         }
 
         private void DeleteSetting(string valueName)
         {
+            TraceStart();
+
             try
             {
                 SettingKey?.DeleteValue(valueName, false);
             }
             catch (Exception e) when (e is SecurityException || e is ObjectDisposedException || e is IOException || e is UnauthorizedAccessException)
             {
+                Tracer?.Write(Category.Debug, e, "Failed to delete value");
             }
+
+            TraceEnd();
         }
 
         private RegistryKey? SettingKey;
+        #endregion
+
+        #region Miscellaneous
+        private void TraceStart([CallerMemberName] string methodName = "")
+        {
+            Debug.Assert(methodName.Length > 0, "Unexpected empty method name");
+
+            Tracer?.Write(Category.Debug, $"{methodName} starting");
+        }
+
+        private void TraceEnd([CallerMemberName] string methodName = "")
+        {
+            Debug.Assert(methodName.Length > 0, "Unexpected empty method name");
+
+            Tracer?.Write(Category.Debug, $"{methodName} done");
+        }
         #endregion
 
         #region Implementation of IDisposable
